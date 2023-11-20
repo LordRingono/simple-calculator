7f051f31ef2350d5d94cc2d5a800ba83f4fe5bc2
LordRingono <40274235+LordRingono@users.noreply.github.com>
2023-11-21 00:00:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int nombre1;
+    int nombre2;
+    int result;
+    printf("Nombre 1 : ");
+    scanf("%d", &nombre1);
+    printf("Nombre 2 : ");
+    scanf("%d", &nombre2);
+    result = nombre1 + nombre2;
+    printf("Hello world!\n%d", result);
     return 0;
 }
