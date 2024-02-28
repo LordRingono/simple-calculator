254fdc84302562bca06ed11176f43241c7235093
LordRingono <40274235+LordRingono@users.noreply.github.com>
2024-02-28 16:33:20 +0000

    ajut de 3 apérations

--- main.c
+++ main.c
@@ -14,6 +14,51 @@ double addition()
     
     return result;
 }
+double soustraction()
+{
+  float nombre1;
+  float nombre2;
+  float result;
+  printf("Nombre 1 : ");
+  scanf("%f", &nombre1);
+  printf("Nombre 2 : ");
+  scanf("%f", &nombre2);
+  result = nombre1 - nombre2;
+
+  return result;
+}
+
+double multiplication()
+{
+  float nombre1;
+  float nombre2;
+  float result;
+  printf("Nombre 1 : ");
+  scanf("%f", &nombre1);
+  printf("Nombre 2 : ");
+  scanf("%f", &nombre2);
+  result = nombre1 * nombre2;
+
+  return result;
+}
+
+double division()
+{
+  float nombre1;
+  float nombre2;
+  float result;
+  printf("Nombre 1 : ");
+  scanf("%f", &nombre1);
+  printf("Nombre 2 : ");
+  scanf("%f", &nombre2);
+  if (nombre2 != 0) {
+    result = nombre1 - nombre2;
+  }
+  else {
+    result = 0;
+  }
+  return result;
+}
 
 int main(int argc, char *argv[])
 {
@@ -24,6 +69,15 @@ int main(int argc, char *argv[])
     switch (choix) {
         case 1 :
             resultat = addition();
+            break;
+        case 2 :
+            resultat = soustraction();
+            break;
+        case 3 :
+            resultat = multiplication();
+            break;
+        case 4 :
+            resultat = division();
             break;
         default:
             break;
